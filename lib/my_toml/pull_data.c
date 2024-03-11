2336a318521ef6f1d0e86e75e47b3b007969be91
57ave <gustave.delecroix@epitech.eu>
2024-03-11 01:09:07 +0100

    fix(lib): change mt_strcmp because of bad return value

--- lib/my_toml/pull_data.c
+++ lib/my_toml/pull_data.c
@@ -5,13 +5,15 @@
 ** pull_data
 */
 
+
+#include <string.h>
 #include "my_toml.h"
 #include "my_lib.h"
 
 void *pull_data(obj_t *obj, char *data_name)
 {
     for (size_t idx = 0; idx < obj->obj_size; idx++) {
-        if (my_strcmp((char const *)data_name
+        if (strcmp((char const *)data_name
         , (char *const) obj->data[idx]->name) == 0) {
             return obj->data[idx]->data;
         }
