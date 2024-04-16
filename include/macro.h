deafebc3c464cad863aa4ec9df08debc0160875d
57ave <gustave.delecroix@epitech.eu>
2024-04-16 13:39:39 +0200

    feat: add include macro.h and toml.h

---
+++ include/macro.h
@@ -0,0 +1,15 @@
+/*
+** EPITECH PROJECT, 2023
+** my_hunter
+** File description:
+** macro
+*/
+
+#ifndef MACRO_H_
+    #define MACRO_H_
+
+    #define EXIT_SUCCESS 0
+    #define EXIT_ERROR 1
+    #define EXIT_FAIL 84
+
+#endif /* !MACRO_H_ */
