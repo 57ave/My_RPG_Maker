693df5fd57f11f5574dbadf593d1962c7acf9e44
57ave <gustave.delecroix@epitech.eu>
2024-04-20 17:36:14 +0200

    feat(lib): change malloc for my_calloc

--- lib/my_realloc.c
+++ lib/my_realloc.c
@@ -14,13 +14,13 @@ void *my_realloc(void *ptr, size_t new_size, size_t old_size)
     void *new_ptr = NULL;
 
     if (ptr == NULL || old_size == 0) {
-        return malloc(new_size);
+        return my_calloc(new_size);
     }
     if (new_size == 0) {
         free(ptr);
         return NULL;
     }
-    new_ptr = malloc(new_size);
+    new_ptr = my_calloc(new_size);
     new_ptr = my_memcpy(new_ptr, ptr, new_size, old_size);
     free(ptr);
     return new_ptr;
