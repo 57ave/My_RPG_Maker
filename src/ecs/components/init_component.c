5d382db046020f6bbcbda055377cb88c1ec2aa1c
57ave <gustave.delecroix@epitech.eu>
2024-04-23 19:32:16 +0200

    feat(ecs/components): add component init gerenic functions that call special function for eatch component

---
+++ src/ecs/components/init_component.c
@@ -0,0 +1,28 @@
+/*
+** EPITECH PROJECT, 2024
+** rpg
+** File description:
+** init_component
+*/
+
+#include "vector.h"
+#include "my_lib.h"
+#include "macro.h"
+#include "components.h"
+#include "ecs.h"
+
+int init_components(
+    struct entity_system *es, void *component,
+    component_t component_type, int entity)
+{
+    es->components[component_type].data =
+        push_index_vector(
+            (vec_t *)es->components[component_type].data, component,
+            COMPONENT_INIT_DATA[component_type].size, entity
+        )
+    ;
+    if (es->components[component_type].data == NULL) {
+        return EXIT_ERROR;
+    }
+    return EXIT_SUCCESS;
+}
