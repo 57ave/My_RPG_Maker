81b2cdd7f1a69c5bf96dd582c570f1a11b735d11
57ave <gustave.delecroix@epitech.eu>
2024-04-26 19:19:31 +0200

    feat(vector): initialize every pointer between data on index pus

--- lib/vector/push_index_vector.c
+++ lib/vector/push_index_vector.c
@@ -9,6 +9,18 @@
 #include "my_lib.h"
 #include <stdlib.h>
 
+void fill_null_data(vec_t *vec, size_t index)
+{
+    void **tmp_data = (void **)vec->data;
+
+    if (index <= vec->size) {
+        return;
+    }
+    for (size_t i = vec->size; i < index; i++) {
+        tmp_data[i] = NULL;
+    }
+}
+
 vec_t *push_index_vector(
     vec_t *vec, void *data, size_t data_size, size_t index)
 {
@@ -25,7 +37,8 @@ vec_t *push_index_vector(
         }
         vec->capacity *= 2;
     }
-    vec->size += 1;
+    fill_null_data(vec, index);
+    vec->size = (vec->size > index) ? index : vec->size;
     tmp_data = (void **)vec->data;
     tmp_data[index] = data;
     return vec;
