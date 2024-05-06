b6d83de2635f1116bb4bf13ab0187339cf694415
57ave <gustave.delecroix@epitech.eu>
2024-05-06 14:20:11 +0200

    feat(ecs/component): update animation struct

--- include/ecs/component/animation_component.h
+++ include/ecs/component/animation_component.h
@@ -7,11 +7,27 @@
 
 #pragma once
 
-typedef struct animation_component {
-    sfIntRect initial_rect;
-    sfIntRect current_rect;
+struct anim_data {
+    sfIntRect rect;
     int x_add;
     int y_add;
     int nb_frame;
     int next_line_frame;
+};
+
+typedef enum direction {
+    LEFT = 0,
+    UP,
+    RIGHT,
+    DOWN,
+    END_DIRECTION
+} direction_t;
+
+typedef struct animation_component {
+    sfIntRect rect;
+    struct anim_data simple_anim;
+    direction_t direction;
+    struct anim_data move_multidir[END_DIRECTION];
+    struct anim_data attack_multidir[END_DIRECTION];
+    struct anim_data
 } c_animation_t;
