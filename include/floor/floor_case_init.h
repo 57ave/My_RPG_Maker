eaa5d2c55f276b35688037ec03e7644a74bf6cff
abeldaverio <abel.daverio@epitech.eu>
2024-05-21 23:51:18 +0200

    feat: wood floor sound

--- include/floor/floor_case_init.h
+++ include/floor/floor_case_init.h
@@ -20,7 +20,7 @@ static const tailed_init_t FLOOR_INIT[] = {
     {"./assets/sprites/grass.png", "./assets/sounds/grass.wav", '#'},
     {"./assets/sprites/sand.png", "./assets/sounds/sand.wav", '+'},
     {"./assets/sprites/water.png", "./assets/sounds/water.wav", '~'},
-    {"./assets/sprites/house_floor.png", "./assets/sounds/water.wav", 'p'},
+    {"./assets/sprites/house_floor.png", "./assets/sounds/wood.wav", 'p'},
 };
 
 static const size_t FLOOR_INIT_SIZE =
