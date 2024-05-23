bb41901c6e683086ea044568a9d6c203858506ec
Yanis <yanis.kernoua@epitech.eu>
2024-05-23 18:04:33 +0200

    fix(ecs/keys): adapted to max gamemode count

--- src/ecs/read_keys.c
+++ src/ecs/read_keys.c
@@ -15,8 +15,10 @@
 
 static void print_gamemode_keys(entity_system_t *es)
 {
-    for (int i = 0; i < 3; ++i) {
-        printf("Mode: %d\n", i);
+    for (int i = 0; i < GAMEMODE_COUNT; ++i) {
+        if (es->keys[i].key_map == NULL)
+            continue;
+        printf("Gamemode: %d\n", es->keys[i].mode);
         for (int j = 0; j < ACTION_COUNT; ++j) {
             printf("Key: %d | Callback: %p\n", es->keys[i].key_map[j]->key,
                 es->keys[i].key_map[j]->callback);
@@ -39,9 +41,11 @@ static void define_key(char *action_name, key_callback_t **key_map,
 
 static int init_keys_gamemode(entity_system_t *es, obj_t *obj, int mode)
 {
-    key_callback_t *key_map[ACTION_COUNT] = {0};
+    key_callback_t **key_map = malloc(sizeof(key_callback_t) * ACTION_COUNT);
     char *action_name = NULL;
 
+    if (key_map == NULL)
+        return EXIT_FAILURE;
     for (int i = 0; i < KEYS_COUNT; ++i) {
         action_name = pull_data(obj, KEY_CONFIGS[i].key_name);
         if (action_name == NULL)
@@ -50,6 +54,7 @@ static int init_keys_gamemode(entity_system_t *es, obj_t *obj, int mode)
             define_key(action_name, key_map, i, j);
         }
     }
+    es->keys[mode].mode = mode;
     es->keys[mode].key_map = key_map;
     return EXIT_SUCCESS;
 }
@@ -63,7 +68,7 @@ static int add_keys_from_path(entity_system_t *es, char const *filepath)
     obj_tab = get_object(content, &offset, obj_tab);
     if (obj_tab == NULL)
         return EXIT_FAILURE;
-    for (int mode = 0; obj_tab[mode] != NULL; mode++) {
+    for (int mode = 0; obj_tab[mode] != NULL; ++mode) {
         if (init_keys_gamemode(es, obj_tab[mode], mode) == EXIT_FAILURE) {
             free_obj(obj_tab);
             return EXIT_FAILURE;
