c0a539ecdcbbcb5b7e93f0b16ec1b99f1185920f
Yanis <yanis.kernoua@epitech.eu>
2024-05-23 13:46:56 +0200

    feat(ecs/events): calling keys mapping callback on keypressed event

--- src/event_loop.c
+++ src/event_loop.c
@@ -11,6 +11,7 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include "components.h"
+#include "key_mapping.h"
 #include "macro.h"
 #include "launcher.h"
 #include "vector.h"
@@ -18,11 +19,25 @@
 #include "filter_entity.h"
 #include "systems.h"
 
+static void handle_keys_event(sfEvent *event, entity_system_t *es)
+{
+    int gamemode = 0; // get_gamemode();
+
+    for (int i = 0; i < KEYS_COUNT; ++i) {
+        if (event->key.code == es->keys[gamemode].key_map[i]->key) {
+            es->keys[gamemode].key_map[i]->callback();
+        }
+    }
+}
+
 static void handle_event(sfRenderWindow *wnd,
-    sfEvent *event, entity_system_t *)
+    sfEvent *event, entity_system_t *es)
 {
     if (event->type == sfEvtClosed) {
         sfRenderWindow_close(wnd);
+    }
+    if (event->type == sfEvtKeyPressed) {
+        handle_keys_event(event, es);
     }
 }
 
