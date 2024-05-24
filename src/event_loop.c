4874e972d39e084431dd7e4233b4a2ea5f888f3c
57ave <gustave.delecroix@epitech.eu>
2024-05-24 07:25:03 +0200

    fix(ecs): event wrongly call here

--- src/event_loop.c
+++ src/event_loop.c
@@ -19,25 +19,11 @@
 #include "filter_entity.h"
 #include "systems.h"
 
-static void handle_keys_event(sfEvent *event, entity_system_t *es)
-{
-    int gamemode = es->game_mode;
-
-    for (int i = 0; i < KEYS_COUNT; ++i) {
-        if (event->key.code == es->keys[gamemode].key_map[i]->key) {
-            es->keys[gamemode].key_map[i]->callback();
-        }
-    }
-}
-
 static void handle_event(sfRenderWindow *wnd,
-    sfEvent *event, entity_system_t *es)
+    sfEvent *event, entity_system_t *)
 {
     if (event->type == sfEvtClosed) {
         sfRenderWindow_close(wnd);
-    }
-    if (event->type == sfEvtKeyPressed) {
-        handle_keys_event(event, es);
     }
 }
 
