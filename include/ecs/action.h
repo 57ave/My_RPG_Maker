f1b700cf6697ce8509dabc58e4eed1bcb0179a9f
57ave <gustave.delecroix@epitech.eu>
2024-05-24 07:27:56 +0200

    feat(ecs/action): add actions function to bind wiht key

---
+++ include/ecs/action.h
@@ -0,0 +1,13 @@
+/*
+** EPITECH PROJECT, 2024
+** rpg
+** File description:
+** action
+*/
+
+#pragma once
+
+int up_action(void *es);
+int down_action(void *es);
+int left_action(void *es);
+int right_action(void *es);
---
+++ src/ecs/action/right.c
@@ -0,0 +1,22 @@
+/*
+** EPITECH PROJECT, 2024
+** rpg
+** File description:
+** up
+*/
+
+#include "components.h"
+#include "ecs.h"
+#include "macro.h"
+
+int right_action(void *es)
+{
+    c_velocity_t *player_vel = get_comp((entity_system_t *)es,
+    VELOCITY, ((entity_system_t *)es)->player);
+    sfInt64 time = (sfTime_asMicroseconds(
+        sfClock_getElapsedTime(player_vel->clock)));
+
+    player_vel->speed.x +=
+        (time * player_vel->velocity.x) / 100000;
+    return EXIT_SUCCESS;
+}
